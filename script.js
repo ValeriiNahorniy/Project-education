5c137907cdd26d6e88cf9850f96b1839f30e7537
Valerii <valeriinagorniy@gmail.com>
2024-06-22 11:41:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// alert('Hello, hello');
+
+// const result = confirm("Are you here?");
+// console. log(result);
+
+// const answer = prompt("Вам 18?", "18");
+// console. log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// let incr = 10,
+//     decr = 10;
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log (5%2);
+
+// 
+
+console.log(2 + 2 * 2 !== '6');
+const isChecked = false,
+isСlose = false;
+
+console.log(isChecked || !isСlose);
